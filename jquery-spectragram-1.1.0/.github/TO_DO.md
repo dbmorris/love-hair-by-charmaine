d02fde67b42aff1447464edb5cf3b5f1672c0438
dbmorris <dbmorris@hotmail.co.uk>
2018-04-28 16:40:28 +0100

    few more tweaks to dividers and styling

--- jquery-spectragram-1.1.0/.github/TO_DO.md
+++
@@ -1,2 +0,0 @@
-- [ ] Create a function to handle error messages $.error in one place
-- [ ] Adjust the img appended to show alt text instead of title
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,38 @@
 jQuery.fn.spectragram.accessData = {
-	accessToken: '640201490.1677ed0.e37bd781535a4ccf8dbe71aec19efd3f'
+  accessToken: '640201490.1677ed0.e37bd781535a4ccf8dbe71aec19efd3f'
 };
 
-$('#instafeed').spectragram('getUserFeed',{
-	max: 6,
-	size: "big",
-	wrapEachWith: '<a>'
+dividerWidth = ($(window).width() - 24);
+
+$(document).ready(function() {
+
+  jQuery.fn.center = function() {
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+      $(window).scrollLeft()) + "px");
+    return this;
+  }
+
+  $('#instafeed').spectragram('getUserFeed', {
+    max: 6,
+    size: "big",
+    wrapEachWith: '<a>'
+  });
+
+  if (dividerWidth < 1200) {
+    $('.divider').width(dividerWidth);
+  }
+
+  $('.divider').center();
+
+  $(window).resize(function() {
+    dividerWidth = ($(window).width() - 24);
+
+    if (dividerWidth < 1200) {
+      $('.divider').width(dividerWidth);
+    }
+
+    $('.divider').center();
+
+  });
+
 });
